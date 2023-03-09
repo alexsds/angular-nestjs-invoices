3892c1ca3860065989af808c9e0bf178e2ae9af5
Alex Kovalenko <iamaleksandr@gmail.com>
2023-03-10 01:33:36 +0200

    Frontend - Integrate with list api

---
+++ frontend/src/environments/environment.api.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  apiUrl: {
+    invoices: {
+      list: 'http://localhost:3000/invoice',
+      one: '/mock/responses/invoices/one.json',
+    },
+  },
+};
