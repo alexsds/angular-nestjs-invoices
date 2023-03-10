25f49a14510e3ee1f4cf21350a7d9feced37fb46
Alex Kovalenko <iamaleksandr@gmail.com>
2023-03-10 19:30:04 +0200

    Frontend - Navigate to main by click on logo

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { InvoiceFormComponent } from './components/invoice-form/invoice-form.component';
 import { ConfirmationModalComponent } from './components/confirmation-modal/confirmation-modal.component';
 
 @NgModule({
   declarations: [NavBarComponent, InvoiceFormComponent, ConfirmationModalComponent],
-  imports: [CommonModule, HttpClientModule, ReactiveFormsModule],
+  imports: [CommonModule, HttpClientModule, ReactiveFormsModule, RouterModule],
   exports: [NavBarComponent, InvoiceFormComponent, ConfirmationModalComponent],
 })
 export class SharedModule {}
