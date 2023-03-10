b5b043ce01deb99b535f2099372f0583f04dfca6
Alex Kovalenko <iamaleksandr@gmail.com>
2023-03-10 11:35:36 +0200

    Frontend - Build reactive form

--- frontend/src/app/shared/components/invoice-form/invoice-form.component.ts
+++ frontend/src/app/shared/components/invoice-form/invoice-form.component.ts
@@ -1,15 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { InvoiceFormService } from '../../services/invoice-form.service';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-invoice-form',
   templateUrl: './invoice-form.component.html',
   styleUrls: ['./invoice-form.component.sass'],
 })
-export class InvoiceFormComponent {
-  constructor(private invoiceFormService: InvoiceFormService) {}
+export class InvoiceFormComponent implements OnInit {
+  form: FormGroup | undefined;
+
+  constructor(private fb: FormBuilder, private invoiceFormService: InvoiceFormService) {}
+
+  ngOnInit(): void {
+    this.createForm();
+  }
 
   onClickOutsideForm() {
     this.invoiceFormService.toggleForm();
+  }
+
+  get items() {
+    return this.form?.get('items') as FormArray;
+  }
+
+  addItem(): void {
+    this.items.push(
+      this.fb.group({
+        name: [''],
+        quantity: [''],
+        price: [''],
+        total: [''],
+      }),
+    );
+  }
+
+  removeItem(index: number) {
+    this.items.removeAt(index);
+  }
+
+  getTotal(index: number): string {
+    const item = this.items.at(index);
+    const quantity = item.get('quantity') ? item.get('quantity')?.value : 0;
+    const price = item.get('price') ? item.get('price')?.value : 0;
+    const totalPrice = quantity * price;
+    const total = parseFloat(String(totalPrice)).toFixed(2);
+    item.get('total')?.setValue(total);
+    return total;
+  }
+
+  onSubmit(): void {
+    const values = this.form?.value;
+    console.log('values', values);
+  }
+
+  private createForm(): void {
+    this.form = this.fb.group({
+      senderAddress: this.fb.group({
+        street: [''],
+        city: [''],
+        postCode: [''],
+        country: [''],
+      }),
+      clientName: [''],
+      clientEmail: [''],
+      clientAddress: this.fb.group({
+        street: [''],
+        city: [''],
+        postCode: [''],
+        country: [''],
+      }),
+      createdAt: [''],
+      paymentTerms: [''],
+      description: [''],
+      items: this.fb.array([]),
+    });
+    this.addItem();
   }
 }
--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { InvoiceFormComponent } from './components/invoice-form/invoice-form.component';
 import { ConfirmationModalComponent } from './components/confirmation-modal/confirmation-modal.component';
 
 @NgModule({
   declarations: [NavBarComponent, InvoiceFormComponent, ConfirmationModalComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   exports: [NavBarComponent, InvoiceFormComponent, ConfirmationModalComponent],
 })
 export class SharedModule {}
