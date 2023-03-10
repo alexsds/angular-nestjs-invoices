eb2df26112751e2814a06012177dfebba5e7a68c
Alex Kovalenko <iamaleksandr@gmail.com>
2023-03-11 00:57:24 +0200

    Frontend - Implement select

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -6,6 +6,7 @@ import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { InvoiceFormComponent } from './components/invoice-form/invoice-form.component';
 import { ConfirmationModalComponent } from './components/confirmation-modal/confirmation-modal.component';
@@ -21,6 +22,7 @@ import { DatepickerCustomHeaderComponent } from './components/datepicker-custom-
     BrowserAnimationsModule,
     MatNativeDateModule,
     MatDatepickerModule,
+    MatSelectModule,
   ],
   exports: [NavBarComponent, InvoiceFormComponent, ConfirmationModalComponent],
 })
