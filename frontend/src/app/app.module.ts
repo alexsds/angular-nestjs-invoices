fb0c290a1d8a1bff491bc998db40188eff02e9c1
Alex Kovalenko <iamaleksandr@gmail.com>
2023-03-10 16:58:53 +0200

    Frontend - Init store

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, SharedModule, StoreModule.forRoot({}, {})],
   providers: [{ provide: DEFAULT_CURRENCY_CODE, useValue: 'GBP' }],
   bootstrap: [AppComponent],
 })
